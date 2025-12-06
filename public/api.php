98c1110336ea3b1961cc2e1fcc150ece8f1c11fe
AntReXcz <katrincsak@seznam.cz>
2025-12-06 22:49:16 +0100

    Persist datastore across API requests

--- public/api.php
+++ public/api.php
@@ -1,14 +1,18 @@
 <?php
 
+session_start();
+
 require_once __DIR__ . '/../app/Model/DataStore.php';
 require_once __DIR__ . '/../app/Model/ProfessionHelper.php';
 require_once __DIR__ . '/../app/Model/MiningService.php';
 require_once __DIR__ . '/../app/Model/CraftingService.php';
 require_once __DIR__ . '/../app/Presenters/ApiPresenter.php';
 
-use App\Presenters\ApiPresenter;
 use App\Model\DataStore;
+use App\Presenters\ApiPresenter;
 
-$presenter = new ApiPresenter(new DataStore());
+$dataStore = $_SESSION['datastore'] ?? new DataStore();
+$presenter = new ApiPresenter($dataStore);
 header('Content-Type: application/json');
 echo $presenter->handle($_REQUEST);
+$_SESSION['datastore'] = $dataStore;
